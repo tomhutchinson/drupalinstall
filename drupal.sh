3175fe2a105c710090019445b5c9ee56a03b5a62
Tom Hutchinson <tmh24@psu.edu>
2014-04-25 14:54:07 -0400

    Change install order, install drush

--- drupal.sh
+++ drupal.sh
@@ -5,11 +5,8 @@ INSTDIR=`pwd`
 # Git
 yum install -y git
 
-# Httpd
-echo "Httpd"
-yum install -y httpd
-chkconfig httpd on
-service httpd restart
+# Gcc
+yum install -y gcc
 
 # MySQL
 echo "MySQL"
@@ -21,6 +18,15 @@ service mysqld restart
 echo "PHP"
 yum install -y php php-devel php-pear
 yum install -y php-mysql php-dom php-gd php-mbstring
+pecl install uploadprogress
+echo "extension=uploadprogress.so"
+
+# Httpd
+echo "Httpd"
+yum install -y httpd
+chkconfig httpd on
+service httpd restart
+
 
 # Drupal
 mv /var/www/html /var/www/html.autobackup
@@ -35,11 +41,27 @@ chown -R drupal:drupal /var/www/html
 chown drupal:drupal /var/www/html/.htaccess
 mkdir /var/www/html/site/default/files
 chown apache:apache files
+chmod -R 755 /var/www/html/sites/all/modules
+chmod -R 755 /var/www/html/sites/all/themes
 
 mysqladmin -u root create drupal
 
 mysql -u root < $INSTDIR\db.sql
 cp $INSTDIR/settings.php /var/www/html/sites/default/settings.php
+chown root:root /var/www/html/sites/default/settings.php
+chmod 644 /var/www/html/sites/default/settings.php
 
 iptables -I INPUT -p tcp -m tcp --dport 80 -j ACCEPT
 iptables -I INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+
+# Drush
+cd /var/www/html
+pear channel-discover pear.drush.org
+pear install drush/drush
+drush
+
+
+
+# Add user to drupal group
+usermod -a -G drupal tmh24
+
