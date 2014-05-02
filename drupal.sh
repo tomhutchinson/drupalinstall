9ae6d77cb98fadc1046885d0414476d763e4fa65
Tom Hutchinson <tmh24@psu.edu>
2014-05-02 10:29:19 -0400

    Database config piped in via include file
    instead of copying the whole honking thing.  Also updated password, and
    automated creation of database tables.

--- drupal.sh
+++ drupal.sh
@@ -31,7 +31,8 @@ mv /var/www/html /var/www/html.autobackup
 git clone http://git.drupal.org/project/drupal.git /var/www/html
 cd /var/www/html
 # TODO - Ask for version
-git checkout 7.27
+read -p "Enter the Drupal version number you want to install:" REPLY1
+git checkout $REPLY1
 
 # Create new user to own Drupal install
 useradd drupal
@@ -47,7 +48,7 @@ mysqladmin -u root create drupal
 
 # Create Drupal database user account, install settings file
 mysql -u root < $INSTDIR/db.sql
-cp $INSTDIR/settings.php /var/www/html/sites/default/settings.php
+cat $INSTDIR/db.inc >> /var/www/html/sites/default/settings.php
 chown root:root /var/www/html/sites/default/settings.php
 chmod 644 /var/www/html/sites/default/settings.php
 
@@ -57,13 +58,26 @@ pear channel-discover pear.drush.org
 pear install drush/drush
 drush > /dev/null
 
+# Create the Drupal database
+drush sql-create --db-su=root
+
 # Add firewall rules for HTTP/HTTPS
 iptables -I INPUT -p tcp -m tcp --dport 80 -j ACCEPT
 iptables -I INPUT -p tcp -m tcp --dport 443 -j ACCEPT
 service iptables restart
 
-# Add user to drupal group - MAKE THIS CUSTOMIZABLE
-usermod -a -G drupal tmh24
+# Add user to drupal group
+while :
+do
+	read -p "Enter a username to grant access to the Drupal install directory, or DONE when finished: " REPLY2
+	if  [ $REPLY2 == 'DONE' ]
+		then
+			break
+		fi
+		#echo $REPLY
+		usermod -a -G drupal $REPLY2
+		
+done
 
 # TODO - Change EnableOverride to All for Clean URLs via .htaccess
 
@@ -86,3 +100,15 @@ drush en -y module_filter
 drush dl pathauto
 drush en -y pathauto
 drush dl site_audit
+
+
+# Done, give feedback
+echo ""
+echo "All Done!"
+echo ""
+echo "!!!!! IMPORTANT !!!!!"
+echo "Your MySQL installation is currently insecure!"
+echo "Be sure to run /usr/bin/mysql_secure_installation to set a MySQL root password and remove the Test database"
+echo ""
+echo "After that, just visit http://<server>/install.php to finish setting up Drupal User 1"
+
